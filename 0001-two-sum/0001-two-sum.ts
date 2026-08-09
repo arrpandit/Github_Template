e8fe37838344b2672ce56522de3d183d5835a8ef
Alok Ranjan Pandit <66639150+arrpandit@users.noreply.github.com>
2026-08-09 11:20:47 +0530

    Time: 0 ms (100%), Space: 57.1 MB (44.34%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -1,12 +1,30 @@
 function twoSum(nums: number[], target: number): number[] {
-    for(let i=0;i<nums.length;i++){
-        for (let j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]==target){
-                return [i,j]
-            }
-        }
-    }
-}
+    // for(let i=0;i<nums.length;i++){
+    //     for (let j=i+1;j<nums.length;j++){
+    //         if(nums[i]+nums[j]==target){
+    //             return [i,j]
+    //         }
+    //     }
+    // }
     //2nd approach
 
+    let helper = new Map<number, number>()
+
+    for (let i=0;i<nums.length;i++){
+        let rem = target - nums[i];
+
+        // if(map.has(rem)){
+        //     return [i, map.get(rem)]
+        // }
+        // map.set(nums[i],i)
+
+        if(helper.has(rem)){
+            return [i, helper.get(rem)]
+        } 
+        helper.set(nums[i],i);
+    }
+
+
+}
+    
     
